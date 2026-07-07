38f39f8a88583a8e9e2d8102967a3fad99bc29f7
Ad1d0z <298187374+Ad1d0z@users.noreply.github.com>
2026-07-07 11:40:41 +0100

    Added a notes api and created some test data

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -8,6 +9,23 @@ app = Flask(__name__)
 @app.route("/api/ping")
 def ping():
     return {"message": "pong"}
+
+@app.route("/api/notes")
+def get_notes():
+    conn = sqlite3.connect("learning-log.db")
+    conn.row_factory = sqlite3.Row
+
+    rows = conn.execute("SELECT * FROM notes ORDER BY created_at DESC").fetchall()
+    conn.close()
+
+    notes = [dict(row) for row in rows]
+    return jsonify(notes)
+
+# A temporary route just to prove the server works.
+# You'll replace this with your real API routes (see the project brief, section 5).
+@app.route("/")
+def default():
+    return {"message": "Hello World"}
 
 
 if __name__ == "__main__":
